bb3fe18bf0b5111568bb076e54c7fecb2ca12242
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-03-22 10:03:39 +0200

    08_json-parse2.py

--- 08_json-parse2.py
+++ 08_json-parse2.py
@@ -1,10 +1,23 @@
-print("URL: " + (url))
+import urllib.parse
+import requests
 
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Kyiv"
+dest = "Kharkiv"
+key = "EYpPUZU66ho8FCpwUSv2WnwKZM2A42Pj"
+
+url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest})
+
+# Print the constructed URL
+print("URL: " + url)
+
+# Parse the JSON data and get the status code
 json_data = requests.get(url).json()
 json_status = json_data["info"]["statuscode"]
 
+# Print the status code and a message if the request is successful
 if json_status == 0:
     print("API Status: " + str(json_status) + " = A successful route call.\n")
-
-
-# ​if json_data['info']['statuscode'] == 0
